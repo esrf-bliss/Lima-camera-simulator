ac086f973e46fb6bd6f9f161fb822654a66b1f92
Samuel Debionne <samuel.debionne@esrf.fr>
2018-07-25 17:00:06 +0200

    Import from lima repo
    Remove deprecated exception specifications
    
    Uniformize include guards
    
    Cleanup and apply clang-format
    
    Add tango device package

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simulator Camera Plugin
+
+This repository aims to get you started with LImA camera plugin development. It provides scaffholding for a camera named MyCamera.
+## Build
+
+### Building against the stable version of LImA
+
+```
+cd conda
+conda build . -c http://bcu-ci.esrf.fr/stable
+```
+
+### Building against the develop version of LImA
+
+```
+cd conda
+conda build . -c http://bcu-ci.esrf.fr/devel
+```
---
+++ include/simulator/SimulatorFrameGetter.h
@@ -0,0 +1,60 @@
+//###########################################################################
+// This file is part of LImA, a Library for Image Acquisition
+//
+// Copyright (C) : 2009-2011
+// European Synchrotron Radiation Facility
+// BP 220, Grenoble 38043
+// FRANCE
+//
+// This is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//###########################################################################
+
+#if !defined(SIMULATOR_FRAMEGETTER_H)
+#define SIMULATOR_FRAMEGETTER_H
+
+#include <lima/SizeUtils.h>
+
+#include "SimulatorCamera.h"
+
+namespace lima {
+
+// Forward definitions
+class FrameDim;
+class Exception;
+
+namespace Simulator {
+
+/// This interface describes a way to get the next frame buffer
+struct FrameGetter {
+
+  virtual Camera::Mode getMode() const = 0;
+
+  virtual void prepareAcq() = 0;
+
+  virtual bool getNextFrame(unsigned char *ptr) = 0;
+
+  virtual unsigned long getFrameNr() const              = 0;
+  virtual void resetFrameNr(unsigned long frame_nr = 0) = 0;
+
+  virtual void setFrameDim(const FrameDim &frame_dim) = 0;
+  virtual void getFrameDim(FrameDim &frame_dim) const = 0;
+
+  virtual void getMaxImageSize(Size &max_image_size) const = 0;
+};
+
+} // namespace Simulator
+
+} // namespace lima
+
+#endif // !defined(SIMULATOR_FRAMEGETTER_H)
