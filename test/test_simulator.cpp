bde1f868047eba7f0007f92ca6d5c2ccfa18286b
Alejandro Homs Puron <ahoms@esrf.fr>
2025-11-07 19:49:57 +0100

    [TEST] allow properly terminating on SIGINT & SIGTERM

--- test/test_simulator.cpp
+++ test/test_simulator.cpp
@@ -22,6 +22,12 @@
 
 #include <cstdlib>
 
+#ifdef __unix
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#endif
+
 #include "simulator/SimulatorInterface.h"
 #include "simulator/SimulatorFrameGetter.h"
 #include "simulator/SimulatorFrameLoader.h"
@@ -143,12 +149,33 @@ void TestApp::configureAcq(const index_map& indexes)
 	DEB_ALWAYS() << DEB_VAR1(effective_dim);
 }
 
+#ifdef __unix
+
+void signal_handler(int sig_no)
+{
+	DEB_GLOBAL_FUNCT();
+	DEB_PARAM() << DEB_VAR1(sig_no);
+
+	TestApp::sendSignal(sig_no);
+}
+
+#endif
 
 int main(int argc, char *argv[])
 {
 	DEB_GLOBAL_FUNCT();
 	std::atexit(PoolThreadMgr::cleanup);
         try {
+#ifdef __unix
+		if (signal(SIGINT, signal_handler) == SIG_ERR)
+			THROW_CTL_ERROR(Error)
+				<< "Error registering SIGINT signal handler: "
+				<< strerror(errno);
+		if (signal(SIGTERM, signal_handler) == SIG_ERR)
+			THROW_CTL_ERROR(Error)
+				<< "Error registering SIGINT signal handler: "
+				<< strerror(errno);
+#endif
 		TestApp app(argc, argv);
 		app.run();
         } catch (Exception& e) {
