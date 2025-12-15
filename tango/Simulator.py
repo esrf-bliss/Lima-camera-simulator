e4fa46f327b871c3c93cd0119ff570bf7789c123
Generic Bliss Account For Control Software <blissadm@linclaustre.esrf.fr>
2025-12-15 16:40:53 +0100

    black passed

--- tango/Simulator.py
+++ tango/Simulator.py
@@ -19,9 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 ############################################################################
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # The Simulator camera plugin TANGO interface
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 import itertools
 import PyTango
@@ -32,14 +32,14 @@ from Lima import Core
 from Lima import Simulator as SimuMod
 
 def grouper(n, iterable, padvalue=None):
-    return zip(*[itertools.chain(iterable, itertools.repeat(padvalue, n-1))]*n)
+    return zip(*[itertools.chain(iterable, itertools.repeat(padvalue, n - 1))] * n)
 
 
 class Simulator(PyTango.Device_4Impl):
 
-#------------------------------------------------------------------
-#    Static properties
-#------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    #    Static properties
+    # ------------------------------------------------------------------
     _SimuCamera = None
     """Reference to Lima simulator camera binding"""
     _SimuInterface = None
@@ -67,11 +67,11 @@ class Simulator(PyTango.Device_4Impl):
 
     Core.DEB_CLASS(Core.DebModApplication, 'LimaSimulator')
 
-#------------------------------------------------------------------
-#    Device constructor
-#------------------------------------------------------------------
-    def __init__(self,*args) :
-        PyTango.Device_4Impl.__init__(self,*args)
+    # ------------------------------------------------------------------
+    #    Device constructor
+    # ------------------------------------------------------------------
+    def __init__(self, *args):
+        PyTango.Device_4Impl.__init__(self, *args)
 
         # Leagcy code used by AttrHelper
         # switch to the new pyTango please
@@ -84,15 +84,15 @@ class Simulator(PyTango.Device_4Impl):
         self.init_device()
         self.set_state(PyTango.DevState.ON)
 
-#------------------------------------------------------------------
-#    Device destructor
-#------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    #    Device destructor
+    # ------------------------------------------------------------------
     def delete_device(self):
         pass
 
-#------------------------------------------------------------------
-#    Device initialization
-#------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    #    Device initialization
+    # ------------------------------------------------------------------
     @Core.DEB_MEMBER_FUNCT
     def init_device(self):
         # Apply properties if any
@@ -113,7 +113,7 @@ class Simulator(PyTango.Device_4Impl):
         if self.peaks:
             peaks = self.peaks
             if isinstance(peaks, str):
-                peaks = peaks.split(',')
+                peaks = peaks.split(",")
             if isinstance(peaks[0], str):
                 peaks = map(float, peaks)
             gauss_peaks = self.getGaussPeaksFromFloatArray(peaks)
@@ -122,7 +122,7 @@ class Simulator(PyTango.Device_4Impl):
         if self.peak_angles:
             peak_angles = self.peak_angles
             if isinstance(peak_angles, str):
-                peak_angles = peak_angles.split(',')
+                peak_angles = peak_angles.split(",")
             if isinstance(peak_angles[0], str):
                 peak_angles = map(float, peak_angles)
             self._SimuCamera.getFrameGetter().setPeakAngles(peak_angles)
@@ -141,7 +141,7 @@ class Simulator(PyTango.Device_4Impl):
         elif depth == 4:
             image_type = Core.Bpp32
         else:
-            raise ValueError('Unknown pixel depth: %d' % depth)
+            raise ValueError("Unknown pixel depth: %d" % depth)
         return Core.FrameDim(width, height, image_type)
 
     @Core.DEB_MEMBER_FUNCT
@@ -158,7 +158,9 @@ class Simulator(PyTango.Device_4Impl):
 
     def __getattr__(self, name):
         try:
-            return AttrHelper.get_attr_4u(self, name, self._SimuCamera.getFrameGetter(), False)
+            return AttrHelper.get_attr_4u(
+                self, name, self._SimuCamera.getFrameGetter(), False
+            )
         except:
             return AttrHelper.get_attr_4u(self, name, self._SimuCamera)
 
@@ -168,38 +170,38 @@ class Simulator(PyTango.Device_4Impl):
         gauss_peaks = [SimuMod.GaussPeak(*p) for p in peak_list]
         return gauss_peaks
 
-    def read_peaks(self,attr):
+    def read_peaks(self, attr):
         gauss_peaks = self._SimuCamera.getFrameGetter().getPeaks()
         peak_list = [(p.x0, p.y0, p.fwhm, p.max) for p in gauss_peaks]
         peak_list_flat = list(itertools.chain(*peak_list))
         attr.set_value(peak_list_flat)
 
-    def write_peaks(self,attr) :
+    def write_peaks(self, attr):
         peak_list_flat = attr.get_write_value()
         gauss_peaks = self.getGaussPeaksFromFloatArray(peak_list_flat)
         self._SimuCamera.getFrameGetter().setPeaks(gauss_peaks)
 
-    def read_peak_angles(self,attr) :
+    def read_peak_angles(self, attr):
         peak_angle_list = self._SimuCamera.getFrameGetter().getPeakAngles()
         attr.set_value(peak_angle_list)
 
-    def write_peak_angles(self,attr) :
+    def write_peak_angles(self, attr):
         peak_angle_list = attr.get_write_value()
         self._SimuCamera.getFrameGetter().setPeakAngles(map(float, peak_angle_list))
 
-    def read_diffraction_pos(self,attr) :
+    def read_diffraction_pos(self, attr):
         x, y = self._SimuCamera.getFrameGetter().getDiffractionPos()
         attr.set_value((x, y))
 
-    def write_diffraction_pos(self,attr) :
+    def write_diffraction_pos(self, attr):
         x, y = attr.get_write_value()
         self._SimuCamera.getFrameGetter().setDiffractionPos(x, y)
 
-    def read_diffraction_speed(self,attr) :
+    def read_diffraction_speed(self, attr):
         sx, sy = self._SimuCamera.getFrameGetter().getDiffractionSpeed()
         attr.set_value((sx, sy))
 
-    def write_diffraction_speed(self,attr) :
+    def write_diffraction_speed(self, attr):
         sx, sy = attr.get_write_value()
         self._SimuCamera.getFrameGetter().setDiffractionSpeed(sx, sy)
 
@@ -211,12 +213,12 @@ class Simulator(PyTango.Device_4Impl):
             nb_prefetched_frames = 0
         attr.set_value(nb_prefetched_frames)
 
-    def read_frame_dim(self,attr) :
+    def read_frame_dim(self, attr):
         frame_dim = self._SimuCamera.getFrameDim()
         dim_arr = self.getLongArrayFromFrameDim(frame_dim)
         attr.set_value(dim_arr)
 
-    def write_frame_dim(self,attr) :
+    def write_frame_dim(self, attr):
         dim_arr = attr.get_write_value()
         frame_dim = self.getFrameDimFromLongArray(dim_arr)
         self._SimuCamera.setFrameDim(frame_dim)
@@ -226,115 +228,93 @@ class SimulatorClass(PyTango.DeviceClass):
     class_property_list = {}
 
     device_property_list = {
-        'frame_dim':
-        [PyTango.DevVarLongArray,
-         "Frame dimension in the form: width, height, depth", []],
-        'pixel_size':
-        [PyTango.DevVarDoubleArray,
-         "Pixel size in the form: width, height (in meter)", []],
-        'mode':
-        [PyTango.DevString,
-         "Simulator mode: GENERATOR, GENERATOR_PREFETCH, LOADER, LOADER_PREFETCH",[]],
-        'nb_prefetched_frames':
-        [PyTango.DevLong,
-         "Number of prefetched frames",[]],
-        'peaks':
-        [PyTango.DevVarDoubleArray,
-         "Gauss peak list [x0,y0,w0,A0,x1,y1,w1,A1...]",[]],
-        'peak_angles':
-        [PyTango.DevVarDoubleArray,
-         "Base rotation angle for each peak",[]],
-        'fill_type':
-        [PyTango.DevString,
-         "Image fill type: GAUSS, DIFFRACTION",[]],
-        'rotation_axis':
-        [PyTango.DevString,
-         "Peak move policy: STATIC, ROTATIONX, ROTATIONY",[]],
-        }
+        "frame_dim": [
+            PyTango.DevVarLongArray,
+            "Frame dimension in the form: width, height, depth",
+            [],
+        ],
+        "pixel_size": [
+            PyTango.DevVarDoubleArray,
+            "Pixel size in the form: width, height (in meter)",
+            [],
+        ],
+        "mode": [
+            PyTango.DevString,
+            "Simulator mode: GENERATOR, GENERATOR_PREFETCH, LOADER, LOADER_PREFETCH",
+            [],
+        ],
+        "nb_prefetched_frames": [PyTango.DevLong, "Number of prefetched frames", []],
+        "peaks": [
+            PyTango.DevVarDoubleArray,
+            "Gauss peak list [x0,y0,w0,A0,x1,y1,w1,A1...]",
+            [],
+        ],
+        "peak_angles": [
+            PyTango.DevVarDoubleArray,
+            "Base rotation angle for each peak",
+            [],
+        ],
+        "fill_type": [PyTango.DevString, "Image fill type: GAUSS, DIFFRACTION", []],
+        "rotation_axis": [
+            PyTango.DevString,
+            "Peak move policy: STATIC, ROTATIONX, ROTATIONY",
+            [],
+        ],
+    }
 
     cmd_list = {
-        'getAttrStringValueList':
-        [[PyTango.DevString, "Attribute name"],
-         [PyTango.DevVarStringArray, "Authorized String value list"]],
+        "getAttrStringValueList": [
+            [PyTango.DevString, "Attribute name"],
+            [PyTango.DevVarStringArray, "Authorized String value list"],
+        ],
         "trigExternal": [[PyTango.DevVoid, ""], [PyTango.DevVoid, ""]],
     }
 
     attr_list = {
         # Simulator mode
-        'frame_dim':
-        [[PyTango.DevLong,
-          PyTango.SPECTRUM,
-          PyTango.READ_WRITE, 3]],
-        'mode':
-        [[PyTango.DevString,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
+        "frame_dim": [[PyTango.DevLong, PyTango.SPECTRUM, PyTango.READ_WRITE, 3]],
+        "mode": [[PyTango.DevString, PyTango.SCALAR, PyTango.READ_WRITE]],
         # Simulator with prefetch
-        'nb_prefetched_frames':
-        [[PyTango.DevLong,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
+        "nb_prefetched_frames": [[PyTango.DevLong, PyTango.SCALAR, PyTango.READ_WRITE]],
         # Simulator in loader mode
-        'file_pattern':
-        [[PyTango.DevString,
-          PyTango.SCALAR,
-          PyTango.WRITE]],
+        "file_pattern": [[PyTango.DevString, PyTango.SCALAR, PyTango.WRITE]],
         # Simulator in generator mode
-        'peaks':
-        [[PyTango.DevDouble,
-          PyTango.SPECTRUM,
-          PyTango.READ_WRITE, 4000]],
-        'peak_angles':
-        [[PyTango.DevDouble,
-          PyTango.SPECTRUM,
-          PyTango.READ_WRITE, 1000]],
-        'grow_factor':
-        [[PyTango.DevDouble,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
-        'fill_type':
-        [[PyTango.DevString,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
-        'rotation_axis':
-        [[PyTango.DevString,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
-        'diffraction_pos':
-        [[PyTango.DevDouble,
-          PyTango.SPECTRUM,
-          PyTango.READ_WRITE, 2]],
-        'diffraction_speed':
-        [[PyTango.DevDouble,
-          PyTango.SPECTRUM,
-          PyTango.READ_WRITE, 2]],
-        'rotation_angle':
-        [[PyTango.DevDouble,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
-        'rotation_speed':
-        [[PyTango.DevDouble,
-          PyTango.SCALAR,
-          PyTango.READ_WRITE]],
-        }
+        "peaks": [[PyTango.DevDouble, PyTango.SPECTRUM, PyTango.READ_WRITE, 4000]],
+        "peak_angles": [
+            [PyTango.DevDouble, PyTango.SPECTRUM, PyTango.READ_WRITE, 1000]
+        ],
+        "grow_factor": [[PyTango.DevDouble, PyTango.SCALAR, PyTango.READ_WRITE]],
+        "fill_type": [[PyTango.DevString, PyTango.SCALAR, PyTango.READ_WRITE]],
+        "rotation_axis": [[PyTango.DevString, PyTango.SCALAR, PyTango.READ_WRITE]],
+        "diffraction_pos": [
+            [PyTango.DevDouble, PyTango.SPECTRUM, PyTango.READ_WRITE, 2]
+        ],
+        "diffraction_speed": [
+            [PyTango.DevDouble, PyTango.SPECTRUM, PyTango.READ_WRITE, 2]
+        ],
+        "rotation_angle": [[PyTango.DevDouble, PyTango.SCALAR, PyTango.READ_WRITE]],
+        "rotation_speed": [[PyTango.DevDouble, PyTango.SCALAR, PyTango.READ_WRITE]],
+    }
 
-    def __init__(self,name) :
-        PyTango.DeviceClass.__init__(self,name)
+    def __init__(self, name):
+        PyTango.DeviceClass.__init__(self, name)
         self.set_type(name)
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 # Plugins
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
 
 def get_control(
-        peaks=[],
-        peak_angles=[],
-        mode=None,
-        _Simulator=Simulator,
-        _Camera=SimuMod.Camera,
-        _Interface=SimuMod.Interface,
-        **kwargs
-    ):
+    peaks=[],
+    peak_angles=[],
+    mode=None,
+    _Simulator=Simulator,
+    _Camera=SimuMod.Camera,
+    _Interface=SimuMod.Interface,
+    **kwargs,
+):
     """
     Called by LimaCCDs module to create Lima control object.
 
